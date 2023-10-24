56ab7df729f80b13c0593f3d351fca6f888f6c2f
ecstatic5 <ecstatic5@users.noreply.github.com>
2023-10-24 17:41:17 +0200

    feat: now returns x number of elements if desired

--- src/Array/methods/choice.lua
+++ src/Array/methods/choice.lua
@@ -1,13 +1,28 @@
 --[[
 	Gets a random element from the Array
 
-	@returns	any		Random element
+	@returns	any | Array		Random element(s)
 ]]
 
-return function(self)
+--[[
+	TODO: Crear función .zip(...)
+]]
+return function(self, amount)
 	local length = self:length()
+	amount = amount or 0
 
+	assert(type(amount) == "number", "'amount' must be a number")
 	assert(length > 0, "Cannot get a random element from an empty array")
+
+	if amount and amount > 0 then
+		local choiced = self.new()
+
+		for _ = 1, amount do
+			choiced:push(self:choice())
+		end
+
+		return choiced
+	end
 
 	return self:at(math.random(1, length))
 end
